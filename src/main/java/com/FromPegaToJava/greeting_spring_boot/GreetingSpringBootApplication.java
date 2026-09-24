d2392b6a267c11884990185676d72b98cbe8073b
kseni <zaytseva9948@gamil.com>
2026-09-24 17:11:16 +0300

    Initial commit

---
+++ src/main/java/com/FromPegaToJava/greeting_spring_boot/GreetingSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.FromPegaToJava.greeting_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreetingSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreetingSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/FromPegaToJava/greeting_spring_boot/GreetingSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.FromPegaToJava.greeting_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreetingSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
